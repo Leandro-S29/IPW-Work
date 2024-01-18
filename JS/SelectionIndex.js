c546c91fbee7b657dfee072ec4957691be063c1e
Leo <151523612+LeoS29-Work@users.noreply.github.com>
2024-01-18 23:45:24 +0000

    Add files via upload

---
+++ JS/SelectionIndex.js
@@ -0,0 +1,11 @@
+function genarateprojeto1() {
+    document.getElementById("projeto").value = "Apanhar Lixo";
+}
+
+function genarateprojeto2() {
+    document.getElementById("projeto").value = "Plantar Arvores";
+}
+
+function genarateprojeto3() {
+    document.getElementById("projeto").value = "Conhecimento da Reciclagem";
+}
